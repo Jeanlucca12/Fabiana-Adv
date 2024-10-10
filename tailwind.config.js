9ca3fdcec024cdcbf24c1c3ec48c057e0465bbf1
jean <jeanluccabueno@gmail.com>
2024-10-09 22:22:54 -0300

    new build

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./**/*.{html,js}"], // Inclui todos os arquivos HTML e JS no projeto
 
   theme: {
     extend: {
       backgroundImage: {
-        "home": "url('/assets/bg-filter.png')",
-      },
-      fontFamily: {
-        sans: ['Merriweather', 'sans-serif'], // Adicionando a fonte 'Poppins'
+        "home": "url('/assets/bg-filter.png')", // Verifique se o caminho da imagem está correto
       },
     },
   },
